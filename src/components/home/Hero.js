8f52d7d4bf6581f28ba066a48220368753222880
Josh Anthony <josh@joshanthony.net>
2022-05-09 19:49:15 +1000

    Hero design changes

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -10,16 +10,16 @@ export default function Hero() {
                 <div className="hero-wrapper grid grid-cols-1 md:grid-cols-10 gap-8 items-center">
                     <div className="hero-text col-span-10 lg:col-span-6 order-2 md:order-1">
                         <h1 className="font-bold text-4xl md:text-5xl max-w-2xl text-slate-100 leading-tight">
-                            Hey, I&apos;m Josh Anthony,
+                            Hi, I&apos;m Josh Anthony,
                         </h1>
                         <h2 className="font-bold text-4xl md:text-5xl max-w-2xl text-slate-300 leading-tight">
                             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-600">
                                 software developer
                             </span> from Brisbane, Australia
                         </h2>
-                        <p className="text-slate-200 text-2xl leading-relaxed mt-8">
-                            I&apos;m a full-stack developer building exceptional digital experiences using technologies such as C# .NET, JavaScript and more.
-                            I&apos;m currently focused on building accessible, human-centered products at one of the largest investment funds in Australia.
+                        <p className="text-slate-200 text-xl leading-relaxed mt-8">
+                            I&apos;m a full-stack developer with experience building digital products using technologies such as C# .NET, JavaScript and more.
+                            I&apos;m currently focused on building accessible, human-centered products at one of Australia's largest investment funds.
                         </p>
                         <a href="https://blog.joshanthony.net" className="inline-flex items-center h-14 px-8 mt-9 text-xl transition-colors duration-150 border border-blue-600 rounded-lg focus:shadow-outline bg-blue-600 text-slate-300 hover:text-slate-200 hover:bg-blue-700">
                             Read my blog
